814759f99e2d60abfb1fb4a933b477ed4f26f744
Matti Luukkainen <mluukkai@iki.fi>
2016-12-30 14:53:44 +0200

    sms

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -5,7 +5,6 @@ class WeatherController < ApplicationController
   end
 
   def sms
-    byebug
     client = EasySMS::Client.new
     resp = client.messages.create(to: '+358405477215', body: 'Hello from Easy SMS Add-on for Heroku.')
     render text: resp.inspect
