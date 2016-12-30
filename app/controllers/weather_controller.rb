68904a55ddc449af56dd105ba12720de9fd0a437
Matti Luukkainen <mluukkai@iki.fi>
2016-12-30 15:03:22 +0200

    weather as sms

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -5,8 +5,9 @@ class WeatherController < ApplicationController
   end
 
   def sms
+    location = Location.of Date.current 
     blowerio = RestClient::Resource.new(ENV['BLOWERIO_URL'])
-    response = blowerio['/messages'].post(:to => '+358405477215', :message => 'Hello from Blower.io')
+    response = blowerio['/messages'].post(:to => '+358405477215', :message => Weather.for(location) )
     render text: response.code
   end
 end
